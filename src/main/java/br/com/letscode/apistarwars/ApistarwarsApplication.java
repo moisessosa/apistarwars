10b36bbbacf589ba27a7645119f255ecae75ff33
moisessosa <rod20028@byui.com>
2022-03-09 18:26:49 -0300

    Initial commit

---
+++ src/main/java/br/com/letscode/apistarwars/ApistarwarsApplication.java
@@ -0,0 +1,13 @@
+package br.com.letscode.apistarwars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApistarwarsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApistarwarsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/letscode/apistarwars/ApistarwarsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.letscode.apistarwars;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApistarwarsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
